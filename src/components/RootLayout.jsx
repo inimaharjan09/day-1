03a46741fbdb60351859208ace43476d25e7e87e
Inima <inima@gmail.com>
2025-03-24 09:08:29 +0545

    changes

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import { Outlet } from 'react-router'
-import Footer from './Footer'
+
 
 export default function RootLayout() {
   return (
@@ -11,7 +11,7 @@ export default function RootLayout() {
     
       <Outlet />
 
-      <Footer/>
+
 
     </div>
   )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Home() {
+    // let count=0;
+    const [count, setCount] = useState(1);
+
+    const handleIncrement =()=>{
+        // count++;
+        //count=9;
+        setCount((count) => count + 1);
+        // console.log(count);
+    };
+    const handleDecrement =()=>{
+        // count++;
+        //count=9;
+        setCount((count) => count - 1);
+        // console.log(count);
+    };
+    
+    // console.log('render');
+    // console.log(count);
   return (
-    <div>
+    <div className='p-4 flex gap-10'>
+        <h1>{count}</h1>
+        <button onClick={handleIncrement} className='bg-black text-white px-2 py-1 cursor-pointer'>Increment</button>
+        <button onClick={handleDecrement} className='bg-black text-white px-2 py-1 cursor-pointer'>Decrement</button>
       
     </div>
   )
